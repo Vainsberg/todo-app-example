abcf4f383152f9ede3c87ead83adf2f83af8df3a
Stas Vasilyev <vasilyev@iMac-Stas.local>
2023-11-01 21:53:42 +0300

    Create generalmap

--- main.go
+++ main.go
@@ -7,12 +7,24 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+var generalmap map[string]string
+
 func get(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintln(w, "GET")
+	value, exists := generalmap["get"]
+	if exists {
+		fmt.Fprintln(w, "Get value:", value)
+
+	} else {
+		fmt.Fprintln(w, "Not found get")
+
+	}
 
 }
 func put(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintln(w, "PUT")
+
+	generalmap["put"] = "This is the PUT handler"
 
 }
 
